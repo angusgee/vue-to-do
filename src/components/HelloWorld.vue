05febdabc0a1725165119cc064b63ee31d874009
MaltaWebDev <MaltaWebDev@gmail.com>
2023-08-31 14:46:56 +0200

    Add project starter files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  data() {
+    return {
+      message: "I love Vue!",
+    };
+  },
+  methods: {
+    clearMessage() {
+      this.message = "";
+    },
+  },
+};
+</script>
+
+<template>
+  <v-container class="pa-5 d-flex flex-column align-center">
+    <input class="mb-5" v-model="message" />
+    <v-btn class="d-inline mb-5" @click="clearMessage()">Clear</v-btn>
+    <!-- <button class="d-inline" @click="message = ''">Clear</button> -->
+    <h5>{{ message }}</h5>
+  </v-container>
+</template>
+
+<style></style>
